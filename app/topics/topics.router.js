0bf8e723d5f8ce0244e623d31a8dba6fbdc04810
Jmoneyyy28 <james.mastrile@gmail.com>
2024-12-06 02:48:59 +0800

    progress

--- app/topics/topics.router.js
+++ app/topics/topics.router.js
@@ -27,9 +27,33 @@ module.exports = (route, controller) => {
             const score = req.body.score;
             const [rows, fields] = await controller.postScore(student_id, quiz_id, score)
             res.send( 'success')
-        }));
-
-        
+        })   
+        .get('/score', async (req, res) => {
+            const student_id = req.query.student_id
+            const [rows, fields] = await controller.getScore(student_id);
+            res.send(rows);
+        })
+        .post('/progress', async (req, res) => {
+            const student_id = req.body.student_id;
+            const progressName = req.body.progressName;
+            const topic_id = req.body.topic_id;
+            console.log(req.body.topic_id);
+            const [rows, fields] = await controller.postProgress(student_id, progressName, topic_id);
+            res.send( 'success')
+        })
+        .get('/studentProgress', async (req, res) => {
+            const student_id = req.query.student_id
+            const progressName = req.body.progressName;
+            const topic_id = req.body.topic_id;
+            const [rows, fields] = await controller.getStudentProgress(student_id, progressName, topic_id);
+            res.send(rows);
+        })
+        .get('/allStudentProgress', async (req, res) => {
+            const teacher_id = req.query.teacher_id;
+            const [rows, fields] = await controller.getAllStudentProgress(teacher_id);
+            res.send(rows);
+        })
+    );
 
     return route;
 };
