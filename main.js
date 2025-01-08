10e52cf6e337a2aa4d625c42e11f12d8e73328ed
maikardan <apple@apples-MacBook-Pro.local>
2025-01-08 19:56:45 +0800

    Pool

--- main.js
+++ main.js
@@ -8,12 +8,25 @@ require("dotenv").config();
 const config = require("./config/config");
 
 // DB Config
+// const dbConnection = {
+// 	host: "45.130.164.28",
+// 	user: "geodev",
+// 	password: "geospheredev",
+// 	database: "geosphere",
+// };
 const dbConnection = {
 	host: "45.130.164.28",
 	user: "geodev",
-	password: "geospheredev",
 	database: "geosphere",
-};
+	password: "geospheredev",
+	waitForConnections: true,
+	connectionLimit: 10,
+	maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+	idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+	queueLimit: 0,
+	enableKeepAlive: true,
+	keepAliveInitialDelay: 0,
+}
 
 // Middlewares
 app.use((req, res, next) => {
